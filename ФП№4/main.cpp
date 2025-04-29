ffd132f8bca31869ded002805368c6265eab4b72
PylaevaSvetlana <pylaeva.sweta@yandex.ru>
2025-04-29 22:55:45 +0300

    Задание 4

---
+++ ФП№4/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <functional>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct less_abs
+{
+	bool operator() (int a, int b) const
+	{
+		return abs(a) < abs(b);
+	}
+};
+
+int main()
+{
+	vector<int> V{ -175, 579, 148, 47,-1};
+	function<bool(int, int)> Func = less_abs();
+	sort(V.begin(), V.end(), Func);
+	for (int i = 0; i != V.size(); i++)
+	{
+		cout << V[i]<< " ";
+	}
+
+	return 0;
+}
